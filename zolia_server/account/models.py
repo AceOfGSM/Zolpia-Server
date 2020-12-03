992ab0c63c7f0bccd3fec8e548b2b40509cca192
hanbin8269 <hanbin8269@gmail.com>
2020-12-03 20:30:41 +0900

    Create User Manger

--- zolia_server/account/models.py
+++ zolia_server/account/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def create_user(self, email, nickname, password=None):
+
+        if not email:
+            raise ValueError("must have user email")
+        user = self.model(email=self.normalize_email(email), nickname=nickname)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password):
+
+        user = self.create_user(
+            email=self.normalize_email(email), nickname=nickname, password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
