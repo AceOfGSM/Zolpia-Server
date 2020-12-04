e7ab1e44f2f934d9c9578d2a2237458f427c2012
hanbin8269 <hanbin8269@gmail.com>
2020-12-04 21:11:32 +0900

    Create VibrationSetting model

--- zolia_server/vibration/models.py
+++ zolia_server/vibration/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class VibrationSetting(models.Model):
+    userID = models.ForeignKey(
+        "account.User", on_delete=models.CASCADE
+    )  # "hanbin8269@gmail.com"
+    name = models.CharField(default="")  # "진동1"
+    isAlarm = models.BooleanField(default=False)  # True
+    alarmTimeTo = models.CharField(null=True)  # "08:00"
