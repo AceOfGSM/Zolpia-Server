ad3976e2f98f25cad7001032dc1b6b41962d1a5f
hanbin8269 <hanbin8269@gmail.com>
2020-12-05 08:36:15 +0900

    Add all ALLOWED_HOSTS

--- zolia_server/config/settings/base.py
+++ zolia_server/config/settings/base.py
@@ -26,7 +26,7 @@ AUTH_USER_MODEL = "account.User"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
