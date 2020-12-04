8d286901eebeb761bfc9938b6b85d8109a146faf
hanbin8269 <hanbin8269@gmail.com>
2020-12-04 17:49:31 +0900

    Create ListCreateSleepAnalysisResultAPI view

--- zolia_server/sleep_analysis/views.py
+++ zolia_server/sleep_analysis/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import SleepAnalysisResultSerializer
+from .models import SleepAnalysisResult
+
+
+class ListCreateSleepAnalysisResultAPI(generics.ListCreateAPIView):
+    queryset = SleepAnalysisResult.objects.all()
+    serializer_class = SleepAnalysisResultSerializer
