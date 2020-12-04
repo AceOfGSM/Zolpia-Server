76230b29bc8af7329e9bd7fbe684987b2e99971f
hanbin8269 <hanbin8269@gmail.com>
2020-12-04 16:07:03 +0900

    Create SleepAnalysisResult Model

--- zolia_server/sleep_analysis/models.py
+++ zolia_server/sleep_analysis/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class SleepAnalysisResult(models.Model):
+    userID = models.ForeignKey("account.User")  # hanbin8269@gmail.com
+    sleepScore = models.FloatField()  # 87
+    sleepTimeFrom = models.CharField(null=True)  # "22:00"
+    sleepTimeTo = models.CharField(null=True)  # "7:00"
+    deepSleepTime = models.IntegerField(null=True)  # 140
+    shallowSleepTime = models.IntegerField(null=True)  # 130
+    evalulation = models.CharField(null=True)  # "나쁘지 않게 주무셨네요 ㅋ"
+    sleepDate = models.DateField()  # "2020-07-18"
