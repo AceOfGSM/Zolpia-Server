574f09229b3f48a5b44b5d88b76b5f9fc87f542c
hanbin8269 <hanbin8269@gmail.com>
2020-12-05 08:55:32 +0900

    Restructure VibrationPattern model

--- zolia_server/vibration/models.py
+++ zolia_server/vibration/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
 
+class VibrationPattern(models.Model):
+    pattern = models.CharField(max_length=256, default="")
+
+
 class VibrationSetting(models.Model):
     userID = models.ForeignKey(
         "account.User", on_delete=models.CASCADE
     )  # "hanbin8269@gmail.com"
+    vibrationPatternID = models.ForeignKey(
+        VibrationPattern, on_delete=models.CASCADE, null=True
+    )
+
     name = models.CharField(default="", max_length=128)  # "진동1"
     isAlarm = models.BooleanField(default=False)  # True
     alarmTimeTo = models.CharField(null=True, max_length=128)  # "08:00"
-
-
-class VibrationPattern(models.Model):
-    vibrationSettingID = models.ForeignKey(VibrationSetting, on_delete=models.CASCADE)
-    value = models.IntegerField()
-    sequence = models.IntegerField()
