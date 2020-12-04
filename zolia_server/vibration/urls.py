8b6298e532b565a0511a69a2f136a9d024c49426
hanbin8269 <hanbin8269@gmail.com>
2020-12-05 08:35:38 +0900

    Add url RetrieveVibrationSettingAPI

--- zolia_server/vibration/urls.py
+++ zolia_server/vibration/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateVibrationSettingAPI
+from .views import CreateVibrationSettingAPI, RetrieveVibrationSettingAPI
 
 urlpatterns = [
     path("/", CreateVibrationSettingAPI.as_view()),
+    path("/<str:name>", RetrieveVibrationSettingAPI.as_view()),
 ]
