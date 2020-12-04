451efb0ac6d64245ffa45975753af1ac0efa6d91
hanbin8269 <hanbin8269@gmail.com>
2020-12-04 17:29:51 +0900

    Add sleep-analysis route in urls.py

--- zolia_server/config/urls.py
+++ zolia_server/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth", include("account.urls")),
-    path("sleep-analysis", include("sleep_analysis")),
+    path("sleep-analysis", include("sleep_analysis.urls")),
 ]
