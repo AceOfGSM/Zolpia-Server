782493d22972ea36b653517bd27fc8983a84691e
hanbin8269 <hanbin8269@gmail.com>
2020-12-05 09:35:06 +0900

    Add ListCreateVibrationPatternAPI url

--- zolia_server/vibration/urls.py
+++ zolia_server/vibration/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ListCreateVibrationSettingAPI, RetrieveVibrationSettingAPI
+from .views import (
+    ListCreateVibrationSettingAPI,
+    RetrieveVibrationSettingAPI,
+    ListCreateVibrationPatternAPI,
+)
 
 urlpatterns = [
     path("/", ListCreateVibrationSettingAPI.as_view()),
     path("/<str:name>", RetrieveVibrationSettingAPI.as_view()),
+    path("/vibration-patterns/", ListCreateVibrationPatternAPI.as_view()),
 ]
