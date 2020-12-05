599b81f65008e4add53b919f628f691a2946019d
hanbin8269 <hanbin8269@gmail.com>
2020-12-05 09:37:01 +0900

    Add RetrieveVibrationPatternAPI url

--- zolia_server/vibration/urls.py
+++ zolia_server/vibration/urls.py
@@ -3,10 +3,12 @@ from .views import (
     ListCreateVibrationSettingAPI,
     RetrieveVibrationSettingAPI,
     ListCreateVibrationPatternAPI,
+    RetrieveVibrationPatternAPI,
 )
 
 urlpatterns = [
     path("/", ListCreateVibrationSettingAPI.as_view()),
     path("/<str:name>", RetrieveVibrationSettingAPI.as_view()),
     path("/vibration-patterns/", ListCreateVibrationPatternAPI.as_view()),
+    path("/vibration-patterns/<str:name>/", RetrieveVibrationPatternAPI.as_view()),
 ]
