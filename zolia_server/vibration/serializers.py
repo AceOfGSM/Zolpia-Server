1439b55a3bf4835731f87fe9937397cec6785e83
hanbin8269 <hanbin8269@gmail.com>
2020-12-05 09:35:14 +0900

    Create VibrationPatternSerializer

--- zolia_server/vibration/serializers.py
+++ zolia_server/vibration/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import VibrationSetting
+from .models import VibrationSetting, VibrationPattern
 
 
 class VibrationSettingSerializer(serializers.ModelSerializer):
     class Meta:
         model = VibrationSetting
+        fields = "__all__"
+
+
+class VibrationPatternSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VibrationPattern
         fields = "__all__"
